47507d5fdfbfe4c712fe78d897244d47e7a770fe
Himavarshini-R <himavarshini.ramachandruni@gmail.com>
2024-07-25 22:03:10 +0530

    Add files via upload

--- MyFirstDjango/calc/urls.py
+++ MyFirstDjango/calc/urls.py
@@ -3,3 +3,7 @@ from . import views
 urlpatterns=[
  path("", views.home, name='home')
 ]
+urlpatterns=[
+ path('',views.home, name='home'),
+ path('add',views.add, name='add')
+]
--- MyFirstDjango/calc/views.py
+++ MyFirstDjango/calc/views.py
@@ -1,4 +1,9 @@
 # Create your views here.
 from django.shortcuts import render
 def home(request):
- return render(request,'home.html',{'name':'Raju'})
+ return render(request,'home.html',{'name':'Varshi'})
+def add(request):
+    val1=int(request.POST['num1'])
+    val2=int(request.POST['num2'])
+    val3=val1+val2
+    return render(request,'result.html',{'result':val3})
